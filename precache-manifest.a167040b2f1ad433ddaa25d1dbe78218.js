76b944f02bf16aacb9bc4cb31dd1c65c0c8b45ca
Servio Palacios <servio@palacios.com>
2019-02-27 17:46:40 -0500

    Updates

---
+++ precache-manifest.a167040b2f1ad433ddaa25d1dbe78218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9839b3189c7f66a58542",
+    "url": "/knowledge-graph/static/css/main.106e40b5.chunk.css"
+  },
+  {
+    "revision": "9839b3189c7f66a58542",
+    "url": "/knowledge-graph/static/js/main.9839b318.chunk.js"
+  },
+  {
+    "revision": "a7f77558f71d96140a77",
+    "url": "/knowledge-graph/static/js/runtime~main.a7f77558.js"
+  },
+  {
+    "revision": "ddd1ba6fc81d8a733f74",
+    "url": "/knowledge-graph/static/js/2.ddd1ba6f.chunk.js"
+  },
+  {
+    "revision": "09f994f3d40fd9ddc53296d8993aee86",
+    "url": "/knowledge-graph/index.html"
+  }
+];
